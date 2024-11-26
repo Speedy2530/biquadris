70b021d3961f040b182e19d7768a73c9e6f8aae5
tle8ball <lotus8ball@gmail.com>
2024-11-25 20:00:02 -0500

    added sblock

---
+++ blocks/sblock.cc
@@ -0,0 +1,31 @@
+#include "sblock.h"
+
+using namespace std;
+
+SBlock::SBlock(bool isCurr) : Block{isCurr}, shape{'T'} {
+    relPos = { {-1, 1}, {-1, 2}, {0, 0}, {0, 1} }  // Imagine the bottom left was {0, 0}
+}
+
+vector <pair <int, int> > SBlock::getRelPos() const override {
+    return relPos;
+}
+
+char SBlock::getShape() const override { return 'T'; }
+
+void SBlock::rotate(string dir) {
+    switch(rotated) {
+        case 0:
+                relPos = { {-2, 0}, {-1, 0}, {-1, 1}, {0, 1} };
+                rotated = 1;
+        case 1:
+                relPos = { {-1, 1}, {-1, 2}, {0, 0}, {0, 1} }; 
+                rotated = 0;
+    }
+}
+// For above  v
+//essentially, when you add the block to the board, just get its Cell values by adding <3, 0>,
+//or whatever the coordinate of the current bottom left is on the board (absRow, absCol).
+
+unique_ptr<Block> clone() const override {
+    return std::make_unique<SBlock>(*this);
+}
---
+++ blocks/sblock.h
@@ -0,0 +1,19 @@
+#ifndef SBLOCK_H
+#define SBLOCK_H
+
+#include "../block.h"
+
+using namespace std;
+
+class SBlock : public Block {
+    char shape;
+
+    public:
+        SBlock(bool isCurr = false);
+        vector <pair <int, int> > getRelPos() const override;
+        char getShape() const override;
+        void rotate(string dir) override;
+        unique_ptr<Block> clone() override;
+};
+
+#endif
