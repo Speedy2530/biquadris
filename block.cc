7e6683b596f831520d8f8917b4e5b7e3786b911d
Saihej Singh <ssaihej@gmail.com>
2024-11-25 21:05:11 -0500

    block print fix

--- block.cc
+++ block.cc
@@ -6,12 +6,13 @@ using namespace std;
 void Block::print() const {
     char grid[4][4] = { {' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' '} };
 
-    for (const auto& pos : relPos) {
-        grid[pos.first + 3][pos.second + 0] = getShape();
+    for (const auto& [relRow, relCol] : relPos) {
+        int newRow = relRow + 3;
+        grid[newRow][relCol] = getShape();
     }
 
     // Print the grid
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
             cout << grid[i][j];
         }
