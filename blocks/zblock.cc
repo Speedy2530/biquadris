47df4f54249139d8ba0929400db9d7697c1a58c2
tle8ball <lotus8ball@gmail.com>
2024-11-25 20:00:18 -0500

    added zblock

---
+++ blocks/zblock.cc
@@ -0,0 +1,31 @@
+#include "zblock.h"
+
+using namespace std;
+
+ZBlock::ZBlock(bool isCurr) : Block{isCurr}, shape{'T'} {
+    relPos = { {-1, 0}, {-1, 1}, {0, 1}, {0, 2} }  // Imagine the bottom left was {0, 0}
+}
+
+vector <pair <int, int> > ZBlock::getRelPos() const override {
+    return relPos;
+}
+
+char ZBlock::getShape() const override { return 'T'; }
+
+void ZBlock::rotate(string dir) {
+    switch(rotated) {
+        case 0:
+                relPos = { {-2, 1}, {-1, 0}, {-1, 1}, {0, 0} };
+                rotated = 1;
+        case 1:
+                relPos = { {-1, 0}, {-1, 1}, {0, 1}, {0, 2} }; 
+                rotated = 0;
+    }
+}
+// For above  v
+//essentially, when you add the block to the board, just get its Cell values by adding <3, 0>,
+//or whatever the coordinate of the current bottom left is on the board (absRow, absCol).
+
+unique_ptr<Block> clone() const override {
+    return std::make_unique<ZBlock>(*this);
+}
---
+++ blocks/zblock.h
@@ -0,0 +1,19 @@
+#ifndef ZBLOCK_H
+#define ZBLOCK_H
+
+#include "../block.h"
+
+using namespace std;
+
+class ZBlock : public Block {
+    char shape;
+
+    public:
+        ZBlock(bool isCurr = false);
+        vector <pair <int, int> > getRelPos() const override;
+        char getShape() const override;
+        void rotate(string dir) override;
+        unique_ptr<Block> clone() override;
+};
+
+#endif
