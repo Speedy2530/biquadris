036b87c8704989a7258267e65947c1d323157621
tle8ball <lotus8ball@gmail.com>
2024-11-25 19:50:44 -0500

    add lblock

---
+++ blocks/lblock.h
@@ -0,0 +1,19 @@
+#ifndef LBLOCK_H
+#define LBLOCK_H
+
+#include "../block.h"
+
+using namespace std;
+
+class LBlock : public Block {
+    char shape;
+
+    public:
+        LBlock(bool isCurr = false);
+        vector <pair <int, int> > getRelPos() const override;
+        char getShape() const override;
+        void rotate(string dir) override;
+        unique_ptr<Block> clone() override;
+};
+
+#endif
