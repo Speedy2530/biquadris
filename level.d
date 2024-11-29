388140700d1366e4a19481b6d181998915b47f80
tle8ball <lotus8ball@gmail.com>
2024-11-28 19:54:15 -0500

    fix memory leak in window.cc

--- level.d
+++
@@ -1,4 +0,0 @@
-level.o: level.cc level.h blocks/all_blocks.h blocks/oblock.h \
- blocks/../block.h blocks/../cell.h blocks/tblock.h blocks/iblock.h \
- blocks/jblock.h blocks/lblock.h blocks/sblock.h blocks/zblock.h \
- blocks/asterisk.h
