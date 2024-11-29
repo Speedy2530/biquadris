165d1406d40839c7a343a748017d70363baed838
s268sing <s268sing@uwaterloo.ca>
2024-11-29 09:23:33 -0500

    force clean

--- blocks/oblock.d
+++
@@ -1,2 +0,0 @@
-blocks/oblock.o: blocks/oblock.cc blocks/oblock.h blocks/../block.h \
- blocks/../cell.h
--- levels/level1.d
+++
@@ -1,7 +0,0 @@
-levels/level1.o: levels/level1.cc levels/level1.h levels/../level.h \
- levels/../blocks/all_blocks.h levels/../blocks/oblock.h \
- levels/../blocks/../block.h levels/../blocks/../cell.h \
- levels/../blocks/tblock.h levels/../blocks/iblock.h \
- levels/../blocks/jblock.h levels/../blocks/lblock.h \
- levels/../blocks/sblock.h levels/../blocks/zblock.h \
- levels/../blocks/asterisk.h levels/../block.h
